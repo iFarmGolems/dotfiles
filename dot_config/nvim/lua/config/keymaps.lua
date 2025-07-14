e4c365dd95d073efa18a634ad3ba44b6f7521611
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-07-14 12:58:49 +0200

    <leader>fc opens chezmoi folder

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -29,3 +29,39 @@ vim.keymap.set("v", "<leader>sra", function()
 end, {
   desc = "Replace visual selection in all files",
 })
+
+vim.keymap.set("n", "<leader>fc", function()
+  local results = require("chezmoi.commands").list({
+    args = {
+      "--path-style",
+      "absolute",
+      "--include",
+      "files",
+      "--exclude",
+      "externals",
+    },
+  })
+  local items = {}
+
+  for _, czFile in ipairs(results) do
+    table.insert(items, {
+      text = czFile,
+      file = czFile,
+    })
+  end
+
+  ---@type snacks.picker.Config
+  local opts = {
+    items = items,
+    confirm = function(picker, item)
+      picker:close()
+      require("chezmoi.commands").edit({
+        targets = { item.text },
+        args = { "--watch" },
+      })
+    end,
+  }
+  Snacks.picker.pick(opts)
+end, {
+  desc = "Chezmoi",
+})
