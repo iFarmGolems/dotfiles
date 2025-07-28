352dc655ed8dfe06d365c258201dcc44a849370d
iFarmGolems <patrikjajcay@gmail.com>
2025-07-28 13:03:35 +0200

    Added apidocs plugin

--- dot_config/nvim/lua/plugins/extra.lua
+++ dot_config/nvim/lua/plugins/extra.lua
@@ -53,4 +53,21 @@ return {
       })
     end,
   },
+
+  {
+    "emmanueltouzery/apidocs.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    cmd = { "ApidocsSearch", "ApidocsInstall", "ApidocsOpen", "ApidocsSelect", "ApidocsUninstall" },
+    config = function()
+      require("apidocs").setup()
+      -- Picker will be auto-detected. To select a picker of your choice explicitly you can set picker by the configuration option 'picker':
+      -- require('apidocs').setup({picker = "snacks"})
+      -- Possible options are 'ui_select', 'telescope', and 'snacks'
+    end,
+    keys = {
+      { "<leader>ad", "<cmd>ApidocsOpen<cr>", desc = "Search Api Doc" },
+    },
+  },
 }
