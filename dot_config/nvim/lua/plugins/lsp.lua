dd79eaf1fed16e24004ef24561aa5eddf9800134
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-08-02 15:12:07 +0200

    Make sure vtsls is not providing trash suggestions

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,26 +1,18 @@
-local nvim_lsp = require("lspconfig")
-
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      inlay_hints = { enabled = false },
       servers = {
-        -- vtsls = {
-        --   single_file_support = false,
-        --   root_dir = nvim_lsp.util.root_pattern("package.json"),
-        --   settings = {
-        --     javascript = {
-        --       suggest = {
-        --         names = false,
-        --         paths = false,
-        --         autoImports = false,
-        --         includeCompletionsForImportStatements = false,
-        --         completeFunctionCalls = false,
-        --       },
-        --     },
-        --   },
-        -- },
+        vtsls = {
+          settings = {
+            javascript = {
+              suggest = {
+                names = false,
+                autoImports = false,
+              },
+            },
+          },
+        },
       },
     },
   },
