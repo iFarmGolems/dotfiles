f8cc2404e82271d87b9dfe7c398ac2c109ea857c
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-08-18 13:37:43 +0200

    Default size of the window: 70%

--- winhome/dot_wezterm.lua
+++ winhome/dot_wezterm.lua
@@ -10,14 +10,24 @@ config.font = wezterm.font_with_fallback({
 	"Symbols Nerd Font Mono",
 })
 
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "TITLE"
 config.window_background_opacity = 0.98
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "NONE"
 config.color_scheme = "Gruvbox dark, hard (base16)"
 
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+	local screen = wezterm.gui.screens().active
+	local ratio = 0.7
+	local width, height = screen.width * ratio, screen.height * ratio
+	local tab, pane, window = wezterm.mux.spawn_window({
+		position = {
+			x = (screen.width - width) / 2,
+			y = (screen.height - height) / 2,
+			origin = "ActiveScreen",
+		},
+	})
+	window:gui_window():set_inner_size(width, height)
 end)
 
 return config
