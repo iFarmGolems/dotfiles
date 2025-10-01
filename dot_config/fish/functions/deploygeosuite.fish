50f011bd6f1ab8d56ed3dd6f2ddf2e4086895459
iFarmGolems <patrikjajcay@gmail.com>
2025-10-01 12:48:40 +0200

    This can accept multiple targets

--- dot_config/fish/functions/deploygeosuite.fish
+++ dot_config/fish/functions/deploygeosuite.fish
@@ -1,15 +1,19 @@
 function deploygeosuite
-    set target $argv[1]
     set local_path ~/develop/repos/mis/sw/ims/ims4/Web/src/main/webapp
     set remote_path /opt/ims/tomcat/webapps/ims
 
-    # Create necessary directories on remote
-    ssh root@$target "mkdir -p $remote_path/js2/fragments/geosuite"
-    ssh root@$target "mkdir -p $remote_path/css2/fragments/geosuite"
+    for target in $argv
+        # Create necessary directories on remote
+        ssh root@$target "mkdir -p $remote_path/js2/fragments/geosuite"
+        ssh root@$target "mkdir -p $remote_path/css2/fragments/geosuite"
 
-    # Transfer the geosuite folder
-    rsync -avz $local_path/js2/fragments/geosuite/ root@$target:$remote_path/js2/fragments/geosuite/
+        # Transfer the geosuite folder
+        rsync -avz $local_path/js2/fragments/geosuite/ root@$target:$remote_path/js2/fragments/geosuite/
 
-    # Transfer the scss file
-    rsync -avz $local_path/css2/fragments/geosuite/geoSuite.scss root@$target:$remote_path/css2/fragments/geosuite/
+        # Transfer the scss file
+        rsync -avz $local_path/css2/fragments/geosuite/geoSuite.scss root@$target:$remote_path/css2/fragments/geosuite/
+
+        # Transfer the thermodiagram library
+        rsync -avz $local_path/build/dist/lib/thermodiagram/ root@$target:$remote_path/build/dist/lib/thermodiagram/
+    end
 end
