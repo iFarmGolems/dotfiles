13b5b48c3c6bfe1e36769b4da6c43da825784d19
iFarmGolems <patrikjajcay@gmail.com>
2025-10-21 13:01:37 +0200

    Commands moved to separate file

---
+++ dot_config/nvim/lua/config/commands.lua
@@ -0,0 +1,65 @@
+vim.api.nvim_create_user_command("CopyPath", function()
+  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  local file_path = vim.fn.expand("%:p")
+  local path_to_copy = file_path
+
+  if vim.v.shell_error == 0 then
+    path_to_copy = file_path:sub(#git_root + 2)
+  end
+
+  vim.fn.setreg("+", path_to_copy)
+  print("Copied: " .. path_to_copy)
+end, {})
+
+vim.api.nvim_create_user_command("EditProjects", function()
+  vim.cmd("edit " .. vim.fn.expand("~/.local/share/nvim/project_nvim/project_history"))
+end, {})
+
+vim.api.nvim_create_user_command("WslView", function()
+  local file_folder = vim.fn.expand("%:p:h")
+  vim.fn.system("wslview " .. file_folder)
+end, {})
+
+vim.api.nvim_create_user_command("SyncLazyVimJson", function()
+  vim.fn.system("cp ~/.config/nvim/lazyvim.json ~/.local/share/chezmoi/dot_config/nvim/lazyvim.json")
+end, {})
+
+vim.api.nvim_create_user_command("SwitchIMSFileType", function()
+  local file_dir = vim.fn.expand("%:p:h")
+  local file_name = vim.fn.expand("%:t")
+  local file_ext = vim.fn.expand("%:e")
+
+  local ims_type = string.match(file_dir, "/webapp/(js2)/")
+    or string.match(file_dir, "/webapp/(html2)/")
+    or string.match(file_dir, "/webapp/(css2)/")
+
+  if ims_type then
+    local valid_extensions = { ".es6.js", ".mjs", ".js", ".html", ".css", ".scss" }
+
+    local matching_folder_types = ims_type == "js2" and { "html2", "css2" }
+      or ims_type == "html2" and { "js2", "css2" }
+      or ims_type == "css2" and { "js2", "html2" }
+      or {}
+
+    -- for each matching type, construct the corresponding file path
+    local paths_to_search = {}
+    for _, mtype in ipairs(matching_folder_types) do
+      local new_path = string.gsub(file_dir, "/" .. ims_type .. "/", "/" .. mtype .. "/")
+      table.insert(paths_to_search, new_path)
+    end
+
+    -- check for each valid extension in the constructed paths
+    for _, base_path in ipairs(paths_to_search) do
+      for _, ext in ipairs(valid_extensions) do
+        local target_file = base_path .. "/" .. string.gsub(file_name, "%." .. file_ext .. "$", ext)
+        if vim.fn.filereadable(target_file) == 1 then
+          vim.cmd("edit " .. target_file)
+          return
+        end
+      end
+    end
+  else
+    print("File not supported")
+    return
+  end
+end, {})
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -20,28 +20,4 @@ vim.o.linespace = 4
 vim.wo.signcolumn = "auto:1"
 
 -- User Commands ---------------------------------------------------------------
-vim.api.nvim_create_user_command("CopyPath", function()
-  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-  local file_path = vim.fn.expand("%:p")
-  local path_to_copy = file_path
-
-  if vim.v.shell_error == 0 then
-    path_to_copy = file_path:sub(#git_root + 2)
-  end
-
-  vim.fn.setreg("+", path_to_copy)
-  print("Copied: " .. path_to_copy)
-end, {})
-
-vim.api.nvim_create_user_command("EditProjects", function()
-  vim.cmd("edit " .. vim.fn.expand("~/.local/share/nvim/project_nvim/project_history"))
-end, {})
-
-vim.api.nvim_create_user_command("WslView", function()
-  local file_folder = vim.fn.expand("%:p:h")
-  vim.fn.system("wslview " .. file_folder)
-end, {})
-
-vim.api.nvim_create_user_command("SyncLazyVimJson", function()
-  vim.fn.system("cp ~/.config/nvim/lazyvim.json ~/.local/share/chezmoi/dot_config/nvim/lazyvim.json")
-end, {})
+require("config.commands")
