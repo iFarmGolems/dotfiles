92f16bee3135a86dc0ad4a95fc83f7ec472f900e
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-10-29 23:15:12 +0100

    Remove padding

--- winhome/dot_wezterm.lua
+++ winhome/dot_wezterm.lua
@@ -15,6 +15,12 @@ config.window_decorations = "NONE"
 config.window_background_opacity = 0.98
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = "Gruvbox dark, hard (base16)"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 wezterm.on("gui-startup", function(cmd)
 	local screen = wezterm.gui.screens().active
