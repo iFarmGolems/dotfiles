a5b3c2492fd346bdf5e810a5f3b48de28ccf03f7
iFarmGolems <patrikjajcay@gmail.com>
2025-11-12 14:49:24 +0100

    Add keybind for switching ims page type

--- dot_config/nvim/lua/config/keymaps/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps/keymaps.lua
@@ -1,3 +1,16 @@
-vim.keymap.set("n", "<leader>xx", "A,<Esc>", { desc = "Add comma to the end of the line" })
+vim.keymap.set(
+	"n",
+	"<leader>xx",
+	"A,<Esc>",
+	{ desc = "Add comma to the end of the line" }
+)
 
+-- delete character without copying to clipboard
 vim.keymap.set("n", "x", '"_x', { desc = "Delete character" })
+
+vim.keymap.set(
+	"n",
+	"<M-o>",
+	":SwitchIMSFileType<CR>",
+	{ silent = true, desc = "Switch IMS page files" }
+)
