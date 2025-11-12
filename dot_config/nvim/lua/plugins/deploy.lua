9a900e5da3521ad94b30ada8c0d3010e8764543d
iFarmGolems <patrikjajcay@gmail.com>
2025-11-12 20:52:09 +0100

    Update to match the library

--- dot_config/nvim/lua/plugins/deploy.lua
+++ dot_config/nvim/lua/plugins/deploy.lua
@@ -1,4 +1,5 @@
 return {
+	-- "iFarmGolems/deploy.nvim",
 	dir = "~/develop/repos/deploy.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-neotest/nvim-nio" },
 	event = "VeryLazy",
@@ -7,23 +8,27 @@ return {
 			timeout = 3,
 			hosts = {
 				{
-					host = "10.111.8.26",
+					address = "10.111.8.26",
 					label = "Bosna 1025 DEV",
 				},
 				{
-					host = "192.168.111.220",
+					address = "172.18.1.235",
+					label = "Bosna 1025 PROD",
+				},
+				{
+					address = "192.168.111.220",
 					label = "Kocka",
 				},
 				{
-					host = "10.111.2.42",
+					address = "10.111.2.42",
 					label = "IMSASAN 991 DEV",
 				},
 				{
-					host = "192.168.151.90",
+					address = "192.168.151.90",
 					label = "IMSASAN 991 PROD",
 				},
 			},
-			mapping = {
+			mappings = {
 				{
 					fs = "/home/patrik/develop/repos/mis/sw/ims/ims4/Web/src/main/webapp",
 					remote = "/opt/ims/tomcat/webapps/ims",
