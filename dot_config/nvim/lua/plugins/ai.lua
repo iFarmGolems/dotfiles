c69282a501df832143e0cccbae34bf2910db1fc9
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-11-23 21:55:25 +0100

    Use codecompanion for now

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -8,6 +8,7 @@ return {
 	{
 		"yetone/avante.nvim",
 		version = false,
+		enabled = false,
 		build = "make",
 		event = "VeryLazy",
 		dependencies = {
@@ -36,5 +37,67 @@ return {
 				auto_set_keymaps = true,
 			},
 		},
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{
+				"<leader>ae",
+				":CodeCompanion ",
+				mode = { "v" },
+				desc = "Code Companion inline edit",
+			},
+			{
+				"<leader>aa",
+				":CodeCompanionChat<CR>",
+				mode = { "n" },
+				silent = true,
+				desc = "Code Companion Chat",
+			},
+		},
+		config = function()
+			require("codecompanion").setup({
+				strategies = {
+					chat = {
+						adapter = "copilot",
+						model = "grok-code-fast-1",
+					},
+				},
+				adapters = {
+					http = {
+						openrouter = function()
+							return require("codecompanion.adapters").extend(
+								"openai_compatible",
+								{
+									env = {
+										url = "https://openrouter.ai/api",
+										api_key = "OPENROUTER_API_KEY",
+										chat_url = "/v1/chat/completions",
+									},
+									handlers = {
+										parse_message_meta = function(self, data)
+											local extra = data.extra
+											if extra and extra.reasoning then
+												data.output.reasoning = { content = extra.reasoning }
+												if data.output.content == "" then
+													data.output.content = nil
+												end
+											end
+											return data
+										end,
+									},
+								}
+							)
+						end,
+					},
+				},
+				opts = {
+					log_level = "DEBUG",
+				},
+			})
+		end,
 	},
 }
