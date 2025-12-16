48480f8c5d2abe9bbdc546061b71b0543783f6d0
iFarmGolems <patrikjajcay@gmail.com>
2025-12-16 15:16:36 +0100

    Remove avante

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -5,39 +5,7 @@ return {
 		event = "InsertEnter",
 		opts = { suggestion = { enabled = true, auto_trigger = true } },
 	},
-	{
-		"yetone/avante.nvim",
-		version = false,
-		enabled = false,
-		build = "make",
-		event = "VeryLazy",
-		dependencies = {
-			"zbirenbaum/copilot.lua",
-		},
-		---@module 'avante'
-		opts = {
-			provider = "copilot",
-			providers = {
-				copilot = { model = "grok-code-fast-1" },
-				openrouter = {
-					__inherited_from = "openai",
-					api_key_name = "OPENROUTER_API_KEY",
-					endpoint = "https://openrouter.ai/api/v1",
-					model = "minimax/minimax-m2",
-				},
-			},
-			web_search_engine = {
-				provider = "tavily",
-			},
-			selection = {
-				hint_display = "none",
-			},
-			behaviour = {
-				auto_add_current_file = false,
-				auto_set_keymaps = true,
-			},
-		},
-	},
+
 	{
 		"olimorris/codecompanion.nvim",
 		dependencies = {
